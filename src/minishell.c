8978a74eb2c8ed58aa79532176cccb0783ad1103
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-08-02 00:01:37 +0300

    fixed hang on exit command caused by an unkilled child

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: zmourtab <zakariamourtaban@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:22:42 by zmourtab          #+#    #+#             */
-/*   Updated: 2024/08/01 23:31:13 by zmourtab         ###   ########.fr       */
+/*   Updated: 2024/08/02 00:00:42 by zmourtab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ void	handle_command(const char *input, char **env)
 	if (pid == -1)
 		printf("error in fork should be doing something here");
 	if (pid == 0)
-		execve(get_path(cmd.cmd[0], env), cmd.cmd, env);
+	{
+		if (execve(get_path(cmd.cmd[0], env), cmd.cmd, env) == -1)
+			printf("command '%s' not found\n",cmd.cmd[0]);
+	}
 	else
-		waitpid(pid,&cmd.status, 0);
+		cmd.pid = pid;
 }
 
 void	process_command(const char *input, char **env)
@@ -35,10 +38,14 @@ void	process_command(const char *input, char **env)
 	if (ft_strncmp(input, "exit", 10) == 0)
 	{
 		printf("Exiting...\n");
+		kill(cmd.pid,SIGINT);
 		exit(0);
 	}
 	else
+	{
 		handle_command(input, env);
+		waitpid(cmd.pid,&cmd.status, 0);
+	}
 }
 
 int	main(int ac, char **av, char **env)
